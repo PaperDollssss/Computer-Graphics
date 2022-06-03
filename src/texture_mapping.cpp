cb406cdb5a00e81814f2d747cebb1b0660f900f3
HRHRHRHR666 <310739976@qq.com>
2022-06-03 12:57:53 +0800

    real arms

--- src/texture_mapping.cpp
+++ src/texture_mapping.cpp
@@ -22,6 +22,7 @@ int _amount = 0;
 float xd = -1.0, yd = 1.0;
 bool knock = true;
 bool knocky = false;
+const glm::mat4 view1;
 
 TextureMapping::TextureMapping(const Options &options) : Application(options)
 {
@@ -166,11 +167,32 @@ TextureMapping::~TextureMapping()
 
 void TextureMapping::handleInput()
 {
+
+
+    double t, k, y = 0;
+    t = (float)glfwGetTime();
+    k = (int)t % 8 + t - (int)t;
+    if (k >= 0 && k < 2)
+        y = (int)t % 2 + t - (int)t;
+    else if (k >= 2 && k < 4)
+        y = 2 - ((int)t % 2 + t - (int)t);
+    else if (k >= 4 && k < 6)
+        y = -((int)t % 2 + t - (int)t);
+    else
+        y = ((int)t % 2 + t - (int)t) - 2;
+
   bool firstMouse = true;
   const float angluarVelocity = 0.1f;
   const float angle = angluarVelocity * static_cast<float>(_deltaTime);
-  const glm::vec3 axis = glm::vec3(0.0f, 1.0f, 0.0f);
+  const float anglel = 0.08 * y;
+  const float angler = -0.08 * y;
+  const glm::vec3 axis = _camera->getRight();
+
   _sphere->rotation = glm::angleAxis(angle, axis) * _sphere->rotation;
+  _arml->rotation = _camera->rotation * glm::angleAxis(anglel, axis);
+  _armr->rotation = _camera->rotation * glm::angleAxis(angler, axis);
+
+
   constexpr float cameraMoveSpeed = 0.05f;
   constexpr float cameraRotateSpeed = 0.02f;
   glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
@@ -338,6 +360,7 @@ void TextureMapping::handleInput()
       camera->rotation =
           glm::quat{(float)cos(thetax / 2), (float)sin(thetax / 2) * right} *
           camera->rotation;
+      //_arml->rotation = camera->rotation;
       _mouseInput.move.xOld = _mouseInput.move.xCurrent;
     }
     if (_mouseInput.move.yCurrent != _mouseInput.move.yOld)
@@ -355,12 +378,12 @@ void TextureMapping::handleInput()
       camera->rotation =
           glm::quat{(float)cos(thetay / 2), (float)sin(thetay / 2) * up} *
           camera->rotation;
+      //_arml->rotation = camera->rotation;
       _mouseInput.move.yOld = _mouseInput.move.yCurrent;
     }
   }
   if (_mouseInput.scroll.y != 0)
   {
-    printf("%f\n", _camera->fovy);
     if (_camera->fovy >= 0.05f && _camera->fovy <= 2.8f)
       _camera->fovy -= 0.02 * _mouseInput.scroll.y;
     if (_camera->fovy <= 0.05f)
@@ -393,6 +416,10 @@ void TextureMapping::renderFrame()
 
   const glm::mat4 projection = _camera->getProjectionMatrix();
   const glm::mat4 view = _camera->getViewMatrix();
+  glm::mat4 view1 = _camera->getViewMatrix();
+
+
+
 
   double t, k, y = 0;
   t = (float)glfwGetTime();
@@ -444,14 +471,13 @@ void TextureMapping::renderFrame()
     _maze->draw();
     _shader->setMat4("model", _ground->getModelMatrix());
     _ground->draw();
-    _arml->position = _camera->position;
-    _arml->rotation = -_camera->rotation;
-    _shader->setMat4("model", _arml->getModelMatrix());
-    _arml->draw();
+
     _armr->position = _camera->position;
-    _armr->rotation = -_camera->rotation;
     _shader->setMat4("model", _armr->getModelMatrix());
     _armr->draw();
+    _arml->position = _camera->position;
+    _shader->setMat4("model", _arml->getModelMatrix());
+    _arml->draw();
 
     if (knock == true)
         _door->position = glm::vec3(2.0f, 2.0f, 2.0f);
